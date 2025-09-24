d13d68663ec1780b1caa8964072611e7813aae49
LordBasuphan <sauljackm@gmail.com>
2025-09-24 16:18:45 +0000

    different types of data types

---
+++ lesson01_data _types.py
@@ -0,0 +1,17 @@
+print(2.0 + 3)
+print(7.0)
+print(6.5 + 2.5)
+
+print(True + True)
+print(1==3)
+print(1!=1)
+print("Hello" != "hello")
+print("9" == "9!") 
+print ("9+6" == "7")
+
+print(type("Yo YO"))
+print(type(7))
+print(type(4.7))
+print(type(4 == 4))
+
+print("Jack\t Age:15\t Scool:Voorhees")
