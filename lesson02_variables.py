8b8857858791d6daac603a3283d1178ffd430e3d
LordBasuphan <sauljackm@gmail.com>
2025-09-25 17:25:18 +0000

    did lesson 2

---
+++ lesson02_variables.py
@@ -0,0 +1,52 @@
+#Concepts Variables
+#Varaibles- Holds value. Stored in the physcial storage of the computer
+
+Password = "Frank1234" 
+email = "sauljackm@gmail.com"
+print("Password:", Password)
+print(Password + " ", email)
+print("Email:", email)
+print(Password, email)
+
+Temperature = 72.5
+print(type(Temperature))
+
+#they can be overwritten
+
+Enabled = False
+is_complete = False 
+print(is_complete)
+is_complete = True
+print(is_complete)
+
+#give them meaningful name
+
+x = 3.14
+y = 7
+print(x + y)
+
+#varaibles are flexible
+count = 10
+print(count)
+count_down = count -1 
+print(count_down)
+count = count_down
+print(count)
+
+Name = "Radia Perlman"
+Age = 34
+Job = "Networking Engineer"
+
+count2 = 10
+count2_up = count2 + 5
+print(count2_up)
+
+x = 4
+y = "Hello"
+switch = x = y 
+print(switch)
+
+
+is_raining = False
+is_raining = True
+print("Is it raining?", is_raining)
