0485d7a2a027d964a6c6a5a61ac87db38688fa1f
LordBasuphan <sauljackm@gmail.com>
2025-09-30 15:53:58 +0000

    did math library

---
+++ lesson04_libraries.py
@@ -0,0 +1,9 @@
+#Python libraries are repositories of code that can be imported into your own repo
+
+import math
+print("\n--- Math Library ---\n")
+print("Square root:", math.sqrt(25))
+print("Round up:", math.ceil(4.1))
+print("Round down:", math.floor(4.5) )
+print("Power:", math.pow(2,5))
+print("Pi:", math.pi)
