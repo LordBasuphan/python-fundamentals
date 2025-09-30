3916b0a79dff4501b71b2a19ed1dcec91c55433e
LordBasuphan <sauljackm@gmail.com>
2025-09-30 16:27:11 +0000

    random number

--- lesson04_libraries.py
+++ lesson04_libraries.py
@@ -1,5 +1,6 @@
 #Python libraries are repositories of code that can be imported into your own repo
 
+#Math library
 import math
 print("\n--- Math Library ---\n")
 print("Square root:", math.sqrt(25))
@@ -7,3 +8,7 @@ print("Round up:", math.ceil(4.1))
 print("Round down:", math.floor(4.5) )
 print("Power:", math.pow(2,5))
 print("Pi:", math.pi)
+
+#Random Library
+#Psuedorandom number generator
+#True random number generator
