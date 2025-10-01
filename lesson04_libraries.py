9d466ba81369a9b92e78791006cbe629ea25ed70
LordBasuphan <sauljackm@gmail.com>
2025-10-01 17:18:33 +0000

    libary

--- lesson04_libraries.py
+++ lesson04_libraries.py
@@ -10,5 +10,11 @@ print("Power:", math.pow(2,5))
 print("Pi:", math.pi)
 
 #Random Library
-#Psuedorandom number generator
-#True random number generator
+#Psuedorandom number generator- Can be random enough. Pick a random number like the date, this is a Seed then divide it by another randomw number and then subtract and add and so on
+#But to be truly random you must use something from real life that is truly random i.e. the temperature a video or a phohto
+#True random number generator- Can only be achived in real life
+
+seed = 182.74
+seedpt2 = math.sqrt(seed)
+seedpt3 = math.pow(seedpt2,5)
+print(seedpt3)
