dfdc5b9eb5703f0d889dcd75ef881766b3e3a8cb
LordBasuphan <sauljackm@gmail.com>
2025-10-03 18:07:06 +0000

    you gotta do better bro

---
+++ lesson05_input.py
@@ -0,0 +1,21 @@
+# name = input("Enter your name: ")
+# print("Hello,",name)
+
+# age = int(input("Your age:"))
+# print(age)
+
+# height = float(input("What is your heigh:"))
+# print("Your height is:", height)
+
+#Challenges 
+# favorite_color = input("What is your favoirte color")
+# print("Your favorite color is:", favorite_color)
+
+# num_1 = int(input("Input a number:"))
+# num_2 = int(input("Input a second number:"))
+# print(num_1 + num_2 )
+
+import math 
+circle_diameter = int(input("Input a number for the diamter of a circle:"))
+circle_radius = circle_diameter / 2
+print(math.pow(circle_radius, 2) * math.pi)
