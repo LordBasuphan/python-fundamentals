30ac9f0df1db3fa21e154834d2a145611f4d0be7
LordBasuphan <sauljackm@gmail.com>
2025-10-07 16:31:00 +0000

    jhuj

---
+++ lesson06_strings.py
@@ -0,0 +1,25 @@
+greetings = "Hello"
+name = "Adda,"
+print(greetings + " " + name)
+message = "you have been selected for jurry duty"
+
+print(greetings + " " + name + " " + message)
+
+print(message[:6])
+
+long_word = "Antidisestablishmentarianism"
+print(long_word[-1:])
+print(long_word[0:28])
+print(long_word[-3::-9])
+print(long_word[:6])
+print(long_word[-8:])
+print(long_word[::2])
+
+country = "Uzbekistan"
+word_length = len(country) 
+print(word_length)
+
+phrase = "JaCkSAuL"
+print("Yo:", phrase.upper)
+print("Yo", phrase.lower)
+print("Yo", phrase.capitalize)
