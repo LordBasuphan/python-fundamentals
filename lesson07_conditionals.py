91ef2b6b807ae4d61ca09a7661bee3c07a9e02c2
LordBasuphan <sauljackm@gmail.com>
2025-10-14 16:25:35 +0000

    h

---
+++ lesson07_conditionals.py
@@ -0,0 +1,41 @@
+# # # print(7 == 8)
+# # # print("Hello" == "Goodbye")
+# # # print(7 != 4)
+# # # print(7 > 3)
+
+# # #If statements 
+# # num = 13
+# # if num == 12: 
+# #     print("The number is 12")
+
+# # if num <= 12: print("The number is less than or equal to 12")
+# # else: print("Thats the wrong number")
+
+# # temperature = 70
+# # if temperature >= 70: print("It is hot")
+# # elif temperature >= 60: print("It is moderate")
+# # else: print("It is cold")
+
+# # x = 2 
+# # y = 20
+# # if x == y: print("X equals y")
+# # elif x >= y: print("X is greater than y")
+# # else: print("X is less than y")
+
+# #Logical operators; and, or, not
+
+# age = 16 
+# has_permisson = True
+
+# if age >= 17 and has_permisson: print("You can drive")
+# elif age == 16 and has_permisson: print("You have a permit")
+# else: print("Get out the car!")
+
+#uisng "or" and "not"
+day = "Monday"
+
+if day == "Saturday" or day == "Sunday": 
+    print("It is the weekend")
+elif day == "Monday" or day == "Tuesday": print("It is Monday or Tuesday")
+else: print("It is a week day")
+if day is not "Monday": "Print it is not Monday"
