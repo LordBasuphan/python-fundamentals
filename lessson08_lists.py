c846ed737083b85db9631243b3c933b4198a25c8
LordBasuphan <sauljackm@gmail.com>
2025-10-16 18:14:05 +0000

    j

--- lessson08_lists.py
+++ lessson08_lists.py
@@ -1,22 +1,41 @@
-# animals = ["donkey", "chicken", "penguin"]
-# numbers = [1, 3, 8, 9]
-# random = [1, "hi", True, 9.99]
-# print("First element;",  animals[0])
-# print("Second element;", animals[1])
-# print("The last element;", animals[-1])
-# animals[0] = "Baboon" 
-# animals.append("Dog")
-# animals.remove("chicken")
+# # animals = ["donkey", "chicken", "penguin"]
+# # numbers = [1, 3, 8, 9]
+# # random = [1, "hi", True, 9.99]
+# # print("First element;",  animals[0])
+# # print("Second element;", animals[1])
+# # print("The last element;", animals[-1])
+# # animals[0] = "Baboon" 
+# # animals.append("Dog")
+# # animals.remove("chicken")
+# # print(animals)
+# # last_animal = animals.pop
+# # empty = []
+# # empty.append("hello")
+# # animal_to_replace = animals.index("donkey")
+# # print(animal_to_replace)
+
+
+
+# nums = [1,2,5,9,18,17,98,34,22,18]
+# print(len(nums))
+# print(min(nums))
+
+# nums.sort()
+# print(nums)
+
+# animals = ["dog", "cat", "sheep", "horse",]
+# if "dog" in animals: print("dog")
+# else: print("No dog")
+
+# copied_list = animals.copy()
+# copied_list.append("pig")
 # print(animals)
-# last_animal = animals.pop
-# empty = []
-# empty.append("hello")
-# animal_to_replace = animals.index("donkey")
-# print(animal_to_replace)
+# print(copied_list)
 
-nums = [1,2,5,9,18,17,98,34,22,18]
-print(len(nums))
-print(min(nums))
+# list_inside = [
+#     [1,2,3,4],
+#     [7,8,10,],
+#     [10.11,45]
+# ]
 
-nums.sort()
-print(nums)
+#Challenges
