f99404b05659d1d4712b305feca75310354416a3
LordBasuphan <sauljackm@gmail.com>
2025-10-16 18:02:05 +0000

    wasd

--- lessson08_lists.py
+++ lessson08_lists.py
@@ -1,12 +1,22 @@
-animals = ["donkey", "chicken", "penguin"]
-numbers = [1, 3, 8, 9]
-random = [1, "hi", True, 9.99]
-print("First element;",  animals[0])
-print("Second element;", animals[1])
-print("The last element;", animals[-1])
-animals[0] = "Baboon" 
-animals.append("Dog")
-animals.insert("Penguin", "Monkey")
-animals.remove("chicken")
-print(animals)
-last_animal = animals.pop
+# animals = ["donkey", "chicken", "penguin"]
+# numbers = [1, 3, 8, 9]
+# random = [1, "hi", True, 9.99]
+# print("First element;",  animals[0])
+# print("Second element;", animals[1])
+# print("The last element;", animals[-1])
+# animals[0] = "Baboon" 
+# animals.append("Dog")
+# animals.remove("chicken")
+# print(animals)
+# last_animal = animals.pop
+# empty = []
+# empty.append("hello")
+# animal_to_replace = animals.index("donkey")
+# print(animal_to_replace)
+
+nums = [1,2,5,9,18,17,98,34,22,18]
+print(len(nums))
+print(min(nums))
+
+nums.sort()
+print(nums)
